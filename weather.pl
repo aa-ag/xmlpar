b28bab8e64c2f0e4154cb9d073404728cdd4442f
aa-ag <aaron.aguerrevere@gmail.com>
2020-12-09 20:43:23 -0600

    new attempt

--- weather.pl
+++ weather.pl
@@ -1,12 +1,33 @@
-#!/usr/bin/perl
-use LWP::Simple;
+#!/usr/bin/env perl
 
-$xml = get "https://w1.weather.gov/xml/current_obs/KORD.xml";
+use strict;
+use warnings 'all';
 
-@lines = split(/\n/, $xml);
+use XML::Twig;
 
-foreach $line (@lines) {
-    if ($line =~ /<([^>]+)>([^<]*)<\/([^>]+)>/) {
-        print "$1: $2\n";
-    }
+my $twig = XML::Twig->parse( \*DATA );
+
+#we can use the 'root' method to find the root of the XML.
+my $root = $twig->root;
+
+#first_child finds the first child element matching a value.
+my $title = $root->first_child('title');
+
+#text reads the text of the element.
+my $title_text = $title->text;
+
+print "Title is: ", $title_text, "\n";
+
+#The above could be combined:
+print $twig ->root->first_child_text('title'), "\n";
+
+## You can use the 'children' method to iterate multiple items:
+my $list = $twig->root->first_child('list');
+
+#children can optionally take an element 'tag' - otherwise it just returns all of them.
+foreach my $element ( $list->children ) {
+
+   #the 'att' method reads an attribute
+   print "Element with ID: ", $element->att('id') // 'none here', " is ", $element->text,
+     "\n";
 }

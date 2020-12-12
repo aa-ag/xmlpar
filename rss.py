2adbc190dcac5f4dd93d051954179ecefeed8552
aa-ag <aaron.aguerrevere@gmail.com>
2020-12-11 20:48:01 -0600

    parsing attempt

--- rss.py
+++ rss.py
@@ -1,11 +1,12 @@
 #--- imports ---#
-import csv
 import requests
 import xml.etree.ElementTree as ET
 
 #--- functions ---#
 
 def load_rss():
+    # create HTTP response object from given url
+    # and creating and saving xml file
     url = 'https://www.prlog.org/news/us/ind/business/rss.xml'
 
     res = requests.get(url)
@@ -13,5 +14,34 @@ def load_rss():
     with open('news.xml', 'wb') as f:
         f.write(res.content)
 
-if __name__ == "__main__":
+
+def parse_xml(xmlfile):
+    # create element tree object
+    # get root element
+    # iterate news items
+
+    tree = ET.parse(xmlfile)
+
+    root = tree.getroot()
+
+    news_items = list()
+
+    for item in root.findall('./channel/item'):
+        news = {}
+
+        for child in item:
+            if child.tag == '{http://search.yahoo.com/mrss/}content':
+                news['media'] = child.attrib['url']
+            else:
+                news[child.tag] = child.text.encode('utf8')
+            
+            items.append(news)
+        print(news_items)
+
+def main():
     load_rss()
+    
+    news_items = parse_xml('news.xml')
+
+if __name__ == "__main__":
+    main()

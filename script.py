b54df390b98099cd411392cbcd3559d7c5adf565
aa-ag <aaron.aguerrevere@gmail.com>
2020-12-11 20:24:53 -0600

    parsing xmpl3

--- script.py
+++ script.py
@@ -1,17 +1,21 @@
 from xml.dom import minidom
 
-doc = minidom.parse("xmpl2.xml")
+doc = minidom.parse("xmpl3.xml")
 
-# doc.getElementsByTagName returns NodeList
-name = doc.getElementsByTagName("name")[0]
-print(name.firstChild.data)
+credit = doc.getElementsByTagName("credit")[0]
+location = doc.getElementsByTagName("location")[0]
+temperature_string = doc.getElementsByTagName("temperature_string")[0]
 
-books = doc.getElementsByTagName("book")
-for book in books:
-        title = book.getElementsByTagName("title")[0]
-        author = book.getElementsByTagName("author")[0]
-        country = book.getAttribute("country")
-        print("Title: %s, Author: %s, Country: %s" % (title.firstChild.data, author.firstChild.data, country) )
+print(credit.firstChild.data)
+print(location.firstChild.data)
+print(temperature_string.firstChild.data)
+
+# books = doc.getElementsByTagName("book")
+# for book in books:
+#         title = book.getElementsByTagName("title")[0]
+#         author = book.getElementsByTagName("author")[0]
+#         country = book.getAttribute("country")
+#         print("Title: %s, Author: %s, Country: %s" % (title.firstChild.data, author.firstChild.data, country) )
 
 '''
 https://github.com/python/cpython/blob/3.9/Lib/xml/dom/minidom.py
